fd6251b0cd7f0a94da4ede2e5bdb03fffe878185
Miha <mobmbsubmail@yahoo.com>
2022-12-19 23:15:31 +0200

    add files

---
+++ src/js/app.js
@@ -0,0 +1,59 @@
+console.log("run");
+
+
+
+$(document).ready(
+    function() {
+        /*
+        nav menu == #nav
+        burget button == #navToggle
+        nav_active - state when menu open
+        get height of hero(first block) attr =  "[data-height='hero']"
+        get height of second block .quote
+        copy nav structure from pug and css
+        */
+        let nav = document.getElementById("nav");
+        let navToggle; //button.burger__menu delete
+        
+        $("#navToggle").on("click", function(event) {
+            event.preventDefalt;
+            console.log(nav.classList);//
+            if (nav.classList.contains('nav_active')) {
+            nav.classList.remove('nav_active');
+            nav.style.height = "auto";
+            console.log("nav.style.height _ " + nav.style.height);//
+        }
+            else {
+            nav.classList.add('nav_active');
+            //let getIntroHeight = document.getElementById("scene").offsetHeight;
+            let firstBlockNameSelector = "[data-height='hero']";
+            let secondBlockClassName = "quote";
+            let getIntroHeight = document.querySelectorAll(firstBlockNameSelector); 
+            
+            getIntroHeight = getIntroHeight[0];
+            getIntroHeight = getIntroHeight.offsetHeight;
+            
+            let getQuoteHeight = document.getElementsByClassName(secondBlockClassName)[0].offsetHeight;
+            //console.log(getIntroHeight);
+            nav.style.height = getIntroHeight - getQuoteHeight + "px"; 
+            }
+            //nav.toggleClass('nav_active');
+        });
+    
+        $("#nav").children().on("click", function (event) {
+            // cancell standart events on click
+            event.preventDefalt;
+            nav.classList.remove('nav_active');
+            nav.style.height = "auto";
+        });
+
+
+
+
+
+
+
+
+});
+
+
